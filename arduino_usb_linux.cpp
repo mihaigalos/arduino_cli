64bca9d5549b42e83d090bcb0d962cff6cdcf910
Mihai Galos <mihaigalos@gmail.com>
2020-04-06 22:01:00 +0200

    Cleanup code, part6

--- arduino_usb_linux.cpp
+++ arduino_usb_linux.cpp
@@ -22,114 +22,129 @@
 #define MODIFY_REPLY_BUFFER 3
 #define RECEIVE_DATA_FROM_PC 4
 
-// used to get descriptor strings for device identification
-static int usbGetDescriptorString(usb_dev_handle *dev, int index, int langid,
-								  char *buf, int buflen)
+class USBDevice
 {
-	char buffer[256];
-	int rval, i;
-
-	// make standard request GET_DESCRIPTOR, type string and given index
-	// (e.g. dev->iProduct)
-	rval = usb_control_msg(dev,
-						   USB_TYPE_STANDARD | USB_RECIP_DEVICE | USB_ENDPOINT_IN,
-						   USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, langid,
-						   buffer, sizeof(buffer), 1000);
-
-	if (rval < 0) // error
-		return rval;
-
-	// rval should be bytes read, but buffer[0] contains the actual response size
-	if ((unsigned char)buffer[0] < rval)
-		rval = (unsigned char)buffer[0]; // string is shorter than bytes read
-
-	if (buffer[1] != USB_DT_STRING) // second byte is the data type
-		return 0;					// invalid return type
-
-	// we're dealing with UTF-16LE here so actual chars is half of rval,
-	// and index 0 doesn't count
-	rval /= 2;
-
-	// lossy conversion to ISO Latin1
-	for (i = 1; i < rval && i < buflen; i++)
+public:
+	static usb_dev_handle *usbOpenDevice(int vendor, char *vendorName, int product,
+										 char *productName)
 	{
-		if (buffer[2 * i + 1] == 0)
-			buf[i - 1] = buffer[2 * i];
-		else
-			buf[i - 1] = '?'; // outside of ISO Latin1 range
-	}
-	buf[i - 1] = 0;
+		struct usb_bus *bus;
+		struct usb_device *dev;
+		char devVendor[256], devProduct[256];
 
-	return i - 1;
-}
+		usb_dev_handle *handle = NULL;
 
-static usb_dev_handle *usbOpenDevice(int vendor, char *vendorName, int product,
-									 char *productName)
-{
-	struct usb_bus *bus;
-	struct usb_device *dev;
-	char devVendor[256], devProduct[256];
+		usb_init();
+		usb_find_busses();
+		usb_find_devices();
 
-	usb_dev_handle *handle = NULL;
-
-	usb_init();
-	usb_find_busses();
-	usb_find_devices();
-
-	for (bus = usb_get_busses(); bus; bus = bus->next)
-	{
-		for (dev = bus->devices; dev; dev = dev->next)
+		for (bus = usb_get_busses(); bus; bus = bus->next)
 		{
-			if (dev->descriptor.idVendor != vendor || dev->descriptor.idProduct != product)
-				continue;
-
-			// we need to open the device in order to query strings
-			if (!(handle = usb_open(dev)))
+			for (dev = bus->devices; dev; dev = dev->next)
 			{
-				fprintf(stderr, "Warning: cannot open USB device: %s\n",
-						usb_strerror());
-				continue;
-			}
+				if (dev->descriptor.idVendor != vendor || dev->descriptor.idProduct != product)
+					continue;
 
-			// get vendor name
-			if (usbGetDescriptorString(handle, dev->descriptor.iManufacturer,
-									   0x0409, devVendor, sizeof(devVendor)) < 0)
-			{
-				fprintf(stderr,
-						"Warning: cannot query manufacturer for device: %s\n",
-						usb_strerror());
-				usb_close(handle);
-				continue;
-			}
+				// we need to open the device in order to query strings
+				if (!(handle = usb_open(dev)))
+				{
+					fprintf(stderr, "Warning: cannot open USB device: %s\n",
+							usb_strerror());
+					continue;
+				}
 
-			// get product name
-			if (usbGetDescriptorString(handle, dev->descriptor.iProduct, 0x0409,
-									   devProduct, sizeof(devVendor)) < 0)
-			{
-				fprintf(stderr,
-						"Warning: cannot query product for device: %s\n",
-						usb_strerror());
-				usb_close(handle);
-				continue;
-			}
+				// get vendor name
+				if (usbGetDescriptorString(handle, dev->descriptor.iManufacturer,
+										   0x0409, devVendor, sizeof(devVendor)) < 0)
+				{
+					fprintf(stderr,
+							"Warning: cannot query manufacturer for device: %s\n",
+							usb_strerror());
+					usb_close(handle);
+					continue;
+				}
 
-			fprintf(stdout, "Found vendor: %s\n", devVendor);
-			fprintf(stdout, "Found product: %s\n\n", devProduct);
+				// get product name
+				if (usbGetDescriptorString(handle, dev->descriptor.iProduct, 0x0409,
+										   devProduct, sizeof(devVendor)) < 0)
+				{
+					fprintf(stderr,
+							"Warning: cannot query product for device: %s\n",
+							usb_strerror());
+					usb_close(handle);
+					continue;
+				}
 
-			if (/*strcmp(devVendor, vendorName) == 0 && */
-				strcmp(devProduct, productName) == 0)
-				return handle;
-			else
-			{
-				usb_close(handle);
-				fprintf(stderr,
-						"Cannot find usb device from : \nVendor: %s\nProduct: %s\n\n",
-						vendorName, productName);
+				fprintf(stdout, "Found vendor: %s\n", devVendor);
+				fprintf(stdout, "Found product: %s\n\n", devProduct);
+
+				if (/*strcmp(devVendor, vendorName) == 0 && */
+					strcmp(devProduct, productName) == 0)
+					return handle;
+				else
+				{
+					usb_close(handle);
+					fprintf(stderr,
+							"Cannot find usb device from : \nVendor: %s\nProduct: %s\n\n",
+							vendorName, productName);
+				}
 			}
 		}
+
+		return NULL;
 	}
 
-	return NULL;
+private:
+	// used to get descriptor strings for device identification
+	static int usbGetDescriptorString(usb_dev_handle *dev, int index, int langid,
+									  char *buf, int buflen)
+	{
+		char buffer[256];
+		int rval, i;
+
+		// make standard request GET_DESCRIPTOR, type string and given index
+		// (e.g. dev->iProduct)
+		rval = usb_control_msg(dev,
+							   USB_TYPE_STANDARD | USB_RECIP_DEVICE | USB_ENDPOINT_IN,
+							   USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, langid,
+							   buffer, sizeof(buffer), 1000);
+
+		if (rval < 0) // error
+			return rval;
+
+		// rval should be bytes read, but buffer[0] contains the actual response size
+		if ((unsigned char)buffer[0] < rval)
+			rval = (unsigned char)buffer[0]; // string is shorter than bytes read
+
+		if (buffer[1] != USB_DT_STRING) // second byte is the data type
+			return 0;					// invalid return type
+
+		// we're dealing with UTF-16LE here so actual chars is half of rval,
+		// and index 0 doesn't count
+		rval /= 2;
+
+		// lossy conversion to ISO Latin1
+		for (i = 1; i < rval && i < buflen; i++)
+		{
+			if (buffer[2 * i + 1] == 0)
+				buf[i - 1] = buffer[2 * i];
+			else
+				buf[i - 1] = '?'; // outside of ISO Latin1 range
+		}
+		buf[i - 1] = 0;
+
+		return i - 1;
+	}
+};
+void showHelp()
+{
+	printf("Usage:\n");
+	printf("arduino_usb_linux on\n");
+	printf("arduino_usb_linux off\n");
+	printf("arduino_usb_linux out\n");
+	printf("arduino_usb_linux write\n");
+	printf("arduino_usb_linux in <string>\n");
+	exit(1);
 }
 
 int main(int argc, char **argv)
@@ -140,16 +155,10 @@ int main(int argc, char **argv)
 
 	if (argc < 2)
 	{
-		printf("Usage:\n");
-		printf("arduino_usb_linux on\n");
-		printf("arduino_usb_linux off\n");
-		printf("arduino_usb_linux out\n");
-		printf("arduino_usb_linux write\n");
-		printf("arduino_usb_linux in <string>\n");
-		exit(1);
+		showHelp();
 	}
 	auto start = std::chrono::high_resolution_clock::now();
-	handle = usbOpenDevice(0x16C0, const_cast<char *>("arduino_usb_linux"), 0x05DC, const_cast<char *>("DotPhat"));
+	handle = USBDevice::usbOpenDevice(0x16C0, const_cast<char *>("arduino_usb_linux"), 0x05DC, const_cast<char *>("DotPhat"));
 
 	if (handle == NULL)
 	{
